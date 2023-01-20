83af91ab6a15aa69ae7d8d60aecfa6703b3d041b
Yasser Shkeir <shkeiryasser@gmail.com>
2023-01-20 21:56:09 +0200

    Leetcode: Pb.1 PHP Solution Cleanup

--- Leetcode/_1_TwoSum/SolutionPHP/Solution.php
+++ Leetcode/_1_TwoSum/SolutionPHP/Solution.php
@@ -2,11 +2,7 @@
 
 class Solution
 {
-    /**
-     * @param Integer[] $nums
-     * @param Integer $target
-     * @return Integer[]
-     */
+
     function twoSum($nums, $target)
     {
         $check = [];
